4e843c493c766611b8066fb20b22976901c82264
perera2001 <nandunperera2001@gmail.com>
2026-08-13 16:54:50 +0530

    main.py updated

--- ai-service/app/main.py
+++ ai-service/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.api.chat_routes import router as chat_router
 from app.api.document_routes import router as document_router
 
 
@@ -12,7 +13,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -20,6 +23,7 @@ app.add_middleware(
 
 
 app.include_router(document_router)
+app.include_router(chat_router)
 
 
 @app.get("/")
